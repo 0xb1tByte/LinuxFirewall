fea537572430aaae015e0c474701acf92b7ba96c
Alaa <42912626+0xb1tByte@users.noreply.github.com>
2020-02-23 19:52:55 +0000

    Create Part2.sh

---
+++ Scripts/Part2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+iptablesPATH=”/sbin/iptables”
+iptablesSavePATH=”/sbin/iptables-save”
+
+# flush all chains
+$iptablesPATH -F 
+# block Access to Port 80
+$iptablesPATH -A INPUT -p tcp --dport 80 -j REJECT
+# Save Settings
+$iptablesSavePATH
