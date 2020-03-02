99f5d0867dfd181d287c0d33c343060fd9ee0292
Alaa <42912626+0xb1tByte@users.noreply.github.com>
2020-03-02 16:49:50 +0000

    Update Part3.sh

--- Scripts/Part3.sh
+++ Scripts/Part3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-iptablesPATH=”/sbin/iptables”
-iptablesSavePATH=”/sbin/iptables-save”
+iptablesPATH="/sbin/iptables"
+iptablesSavePATH="/sbin/iptables-save"
 
 # flush all chains
 $iptablesPATH -F 
@@ -9,5 +9,7 @@ $iptablesPATH -F
 $iptablesPATH -P INPUT DROP
 # Exception to Default Deny
 $iptablesPATH -A INPUT -p tcp --dport 22 -j ACCEPT # SSH
+# Accept the packets that are related to the connectins established by the server 
+$iptablesPATH -A INPUT --match state --state ESTABLISHED,RELATED --jump ACCEPT
 # Save Settings
 $iptablesSavePATH
