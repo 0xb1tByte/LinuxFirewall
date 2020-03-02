6e9fa7cdcecb7d3e823ba3dedb58e742cbf0a19e
Alaa <42912626+0xb1tByte@users.noreply.github.com>
2020-03-02 17:47:35 +0000

    Update Part4-2.sh

--- Scripts/Part4-2.sh
+++ Scripts/Part4-2.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-iptablesPATH=”/sbin/iptables”
-iptablesSavePATH=”/sbin/iptables-save”
-ServerIP=”192.168.101.2”
+iptablesPATH="/sbin/iptables"
+iptablesSavePATH="/sbin/iptables-save"
+ServerIP=192.168.101.2
 
 # flush all chains
 $iptablesPATH -F 
+# FORWARD all packets
 # Block outgoing connection to 192.168.101.2 on port 80 
-$iptablesPATH -A FORWARD -d $ServerIP -p tcp –dport 80 -j DROP
+$iptablesPATH -A FORWARD -d $ServerIP -p tcp --dport 80 -j DROP
 # Save Settings
 $iptablesSavePATH
