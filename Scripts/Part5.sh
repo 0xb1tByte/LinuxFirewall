d21502b9d6a9042dcef70f298be2ac35aa71aa33
Alaa <42912626+0xb1tByte@users.noreply.github.com>
2020-03-03 20:55:54 +0000

    Update Part5.sh

--- Scripts/Part5.sh
+++ Scripts/Part5.sh
@@ -2,41 +2,20 @@
 
 iptablesPATH="/sbin/iptables"
 iptablesSavePATH="/sbin/iptables-save"
-ServerIP="192.168.101.2"
-ClientIP="192.168.100.2"
-Server-Net="192.168.101.0/24" 
-Client-Net="192.168.100.0/24"
-Client-Net_Interface="enp0s3"
-Server-Net_Interface="enp0s8"
-routerInterfaces="${Server-Net_Interface} ${Client-Net_Interface}"
-PrivateIPs="10.0.0.0/8 172.16.0.0/12 192.168.0.0/16"
+Client_Net_Interface="enp0s8"
+Server_Net_Interface="enp0s9"
 
 # flush all chains
 $iptablesPATH -F 
 
-# DROP all packet that are coming from routerInterfaces with source and destination as $ServerIP 
-for interface in $routerInterfaces
-do
-  $iptablesPATH -A FORWARD -i $interface -d $ServerIP -s  $ServerIP -j DROP
-  $iptablesPATH -A FORWARD -o $interface -d $ServerIP -s  $ServerIP -j DROP
-done
+# Drop all FORWARD packet 
+$iptablesPATH -P FORWARD DROP
 
-# DROP Private IPs Packets
-for ip in $PrivateIPs
-do
-  for interface in $routerInterfaces
-  do
-  $iptablesPATH -A FORWARD -o $interface  -s $ip -j DROP
-  $iptablesPATH -A FORWARD -i $interface  -s $ip -j DROP
-  done
-done
+# Exception to last policy , allow trafic from Server_Net_Interface => Client_Net_Interface
+$iptablesPATH -A FORWARD -i $Server_Net_Interface -o $Client_Net_Interface -j ACCEPT
 
-# DROP all packet going to Server except packet that are coming from Client Networks
-$iptablesPATH -A FORWARD -d $ServerIP -s !$Client-Net -j DROP
-
-# Prevent the server from sending spoofed packets 
-$iptablesPATH -A FORWARD -o $Server-Net_Interface -s !$ServerIP -j DROP
-
+# Exception to last policy , allow trafic from Client_Net_Interface => Server_Net_Interface 
+$iptablesPATH -A FORWARD -i $Client_Net_Interface -o $Server_Net_Interface -j ACCEPT
 
 # Save Settings
 $iptablesSavePATH
