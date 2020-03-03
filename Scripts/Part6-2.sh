4dbdfd858d21d56a98d0ad21a51e449438267746
Alaa <42912626+0xb1tByte@users.noreply.github.com>
2020-03-03 22:03:21 +0000

    Update Part6-2.sh

--- Scripts/Part6-2.sh
+++ Scripts/Part6-2.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-iptablesPATH=”/sbin/iptables”
-iptablesSavePATH=”/sbin/iptables-save”
-ServerIP=”192.168.101.2”
+iptablesPATH="/sbin/iptables"
+iptablesSavePATH="/sbin/iptables-save"
+ServerIP="192.168.101.2"
+Client_Net_Interface="enp0s8"
+Server_Net_Interface="enp0s9"
 
 # flush all chains
 $iptablesPATH -F
 
+
 # Create a new chain called LOGGING
 $iptablesPATH -N LOGGING
-$iptablesPATH -A LOGGING -j LOG --m hashlimit --hashlimit 1/sec --hashlimit-mode dstip,dstport,srcip,srcport --log-prefix "LOG: Connection to 80 dropped" --log-level 6
+$iptablesPATH -A LOGGING -j LOG -m hashlimit --hashlimit 1/sec --hashlimit-mode dstip,dstport,srcip,srcport --hashlimit-name hosts --log-prefix "LOGDROPPED: Connection to 80 dropped" --log-level 6
 $iptablesPATH -A LOGGING -j DROP
 
- # Block outgoing connection to 192.168.101.2 on port 80 
-$iptablesPATH -A FORWARD -d $ServerIP -p tcp –dport 80 -j LOGGING
+ # Block incoming connection to 192.168.101.2 on port 80 
+$iptablesPATH -A FORWARD -i $Client_Net_Interface -o $Server_Net_Interface -d $ServerIP -p tcp --dport 80 -j LOGGING
+ 
+
 
 # Save Settings
 $iptablesSavePATH
