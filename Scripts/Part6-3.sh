cdaec059183525b32b8717da900e2cf68fdcc3b0
Alaa <42912626+0xb1tByte@users.noreply.github.com>
2020-03-04 19:42:42 +0000

    Update Part6-3.sh

--- Scripts/Part6-3.sh
+++ Scripts/Part6-3.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-iptablesPATH=”/sbin/iptables”
-iptablesSavePATH=”/sbin/iptables-save”
-ServerIP=”192.168.101.2”
+iptablesPATH="/sbin/iptables"
+iptablesSavePATH="/sbin/iptables-save"
+ServerIP="192.168.101.2"
+Client_Net_Interface="enp0s8"
+Server_Net_Interface="enp0s9"
 
 # flush all chains
-$iptablesPATH -F 
+$iptablesPATH -F
 
-# Create a new chain called LOGGING
-$iptablesPATH -N LOGGING
-$iptablesPATH -A LOGGING -j LOG --m hashlimit --hashlimit 1/sec --hashlimit-mode dstip,dstport,srcip,srcport --log-prefix "LOG: Connection to 80 dropped" --log-level 6
-$iptablesPATH -A LOGGING -j DROP
+# Create a new chain called LOGGING2
+$iptablesPATH -N LOGGING2
+$iptablesPATH -A LOGGING2 -j LOG -m hashlimit --hashlimit 1/sec --hashlimit-mode dstip,dstport,srcip,srcport --hashlimit-name hosts --log-prefix "LOGDROPPED2:dropped!!" --log-level 6
+$iptablesPATH -A LOGGING2 -j DROP
+
+# Accept the packets that are related to the connectins established by the server 
+$iptablesPATH -A FORWARD -i $Client_Net_Interface -d $ServerIP --match state --state ESTABLISHED,RELATED --jump ACCEPT
+
+# Accept connection on 22 port (SSH)
+$iptablesPATH -A FORWARD -i $Client_Net_Interface -o $Server_Net_Interface -d $ServerIP -p tcp --dport 22 -j ACCEPT 
 
 # Default Deny to outgoing connection to 192.168.101.2 on all services
-$iptablesPATH -A FORWARD -d $ServerIP -j LOGGING
-# Exception to previous policy 
-$iptablesPATH -A FORWARD -d $ServerIP  -p tcp --dport 22 -j ACCEPT # SSH
+$iptablesPATH -A FORWARD -i $Client_Net_Interface -o $Server_Net_Interface -d $ServerIP -j LOGGING2
+
 
 # Save Settings
 $iptablesSavePATH
