4a428cfdf61b4d1e19f69c26f17651b9e56003fb
Alaa <42912626+0xb1tByte@users.noreply.github.com>
2020-03-06 18:18:02 +0000

    Update Part5.sh

--- Scripts/Part5.sh
+++ Scripts/Part5.sh
@@ -11,10 +11,10 @@ $iptablesPATH -F
 # Drop all FORWARD packet 
 $iptablesPATH -P FORWARD DROP
 
-# Exception to last policy , allow trafic from Server_Net_Interface => Client_Net_Interface
+# Exception to the policy , allow trafic from Server_Net_Interface => Client_Net_Interface
 $iptablesPATH -A FORWARD -i $Server_Net_Interface -o $Client_Net_Interface -j ACCEPT
 
-# Exception to last policy , allow trafic from Client_Net_Interface => Server_Net_Interface 
+# Exception to the policy , allow trafic from Client_Net_Interface => Server_Net_Interface 
 $iptablesPATH -A FORWARD -i $Client_Net_Interface -o $Server_Net_Interface -j ACCEPT
 
 # Save Settings
