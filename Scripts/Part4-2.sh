6e3f490822fc5914cf53a22945f72fd8546d50ac
Alaa <42912626+0xb1tByte@users.noreply.github.com>
2020-03-06 13:32:05 +0000

    Update Part4-2.sh

--- Scripts/Part4-2.sh
+++ Scripts/Part4-2.sh
@@ -6,7 +6,6 @@ ServerIP=192.168.101.2
 
 # flush all chains
 $iptablesPATH -F 
-# FORWARD all packets
 # Block outgoing connection to 192.168.101.2 on port 80 
 $iptablesPATH -A FORWARD -d $ServerIP -p tcp --dport 80 -j DROP
 # Save Settings
